65bd4722cfdc7b78a9d8e9e47565cfd0d519b21a
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-20 15:53:34 -0500

    AnimeRepository creado

---
+++ src/main/java/com/carlos/animeapi/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package com.carlos.animeapi.repository;
+
+import com.carlos.animeapi.model.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+}
