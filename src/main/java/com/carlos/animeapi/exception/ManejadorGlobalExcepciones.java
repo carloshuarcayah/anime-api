3ce7ab7c9c021aaa1da0fb6ff7470f56b0521b51
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-24 15:24:53 -0500

    Manejo de validaciones agregado

--- src/main/java/com/carlos/animeapi/exception/ManejadorGlobalExcepciones.java
+++ src/main/java/com/carlos/animeapi/exception/ManejadorGlobalExcepciones.java
@@ -1,7 +1,10 @@
 package com.carlos.animeapi.exception;
 
+import jakarta.xml.bind.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,6 +14,7 @@ import java.util.Objects;
 
 @ControllerAdvice
 public class ManejadorGlobalExcepciones {
+    //POR SI NOI SE ENCUENTRA ALGO QUE BUSCAMOS
     @ExceptionHandler(RecursoNoEncontradoException.class)
     public ResponseEntity<Object> manejarRecursoNoEncontrado(RecursoNoEncontradoException ex){
         Map<String, String> respuesta = new HashMap<>();
@@ -18,6 +22,18 @@ public class ManejadorGlobalExcepciones {
         return new ResponseEntity<>(respuesta, HttpStatus.NOT_FOUND);
     }
 
+    //PARA LOS ERRORES DE LOS VALID
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> manejarValidaciones(MethodArgumentNotValidException ex){
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("excepcion","Errores de validacion");
+        ex.getBindingResult().getAllErrors().forEach(error->{
+            respuesta.put(((FieldError)error).getField(),"Datos no validos: "+error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(respuesta,HttpStatus.BAD_REQUEST);
+    }
+
+    //PARA CUALQUIER OTRO ERROR
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> manejarExcepcionesGenerales(Exception ex){
         Map<String, String> respuesta = new HashMap<>();
