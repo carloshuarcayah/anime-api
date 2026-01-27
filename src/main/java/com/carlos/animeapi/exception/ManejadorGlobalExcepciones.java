ac1f91931e717f0a43dcf062739906097041d180
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-27 11:58:26 -0500

    Manejador de errores en parametros agregado

--- src/main/java/com/carlos/animeapi/exception/ManejadorGlobalExcepciones.java
+++ src/main/java/com/carlos/animeapi/exception/ManejadorGlobalExcepciones.java
@@ -7,6 +7,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,21 +17,28 @@ import java.util.Objects;
 public class ManejadorGlobalExcepciones {
     //POR SI NOI SE ENCUENTRA ALGO QUE BUSCAMOS
     @ExceptionHandler(RecursoNoEncontradoException.class)
-    public ResponseEntity<Object> manejarRecursoNoEncontrado(RecursoNoEncontradoException ex){
+    public ResponseEntity<Object> manejarRecursoNoEncontrado(RecursoNoEncontradoException ex) {
         Map<String, String> respuesta = new HashMap<>();
-        respuesta.put("mensaje",ex.getMessage());
+        respuesta.put("mensaje", ex.getMessage());
         return new ResponseEntity<>(respuesta, HttpStatus.NOT_FOUND);
     }
 
     //PARA LOS ERRORES DE LOS VALID
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> manejarValidaciones(MethodArgumentNotValidException ex){
+    public ResponseEntity<Object> manejarValidaciones(MethodArgumentNotValidException ex) {
         Map<String, String> respuesta = new HashMap<>();
-        respuesta.put("excepcion","Errores de validacion");
-        ex.getBindingResult().getAllErrors().forEach(error->{
-            respuesta.put(((FieldError)error).getField(),"Datos no validos: "+error.getDefaultMessage());
+        respuesta.put("excepcion", "Errores de validacion");
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            respuesta.put(((FieldError) error).getField(), "Datos no validos: " + error.getDefaultMessage());
         });
-        return new ResponseEntity<>(respuesta,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(respuesta, HttpStatus.BAD_REQUEST);
+    }
+    //PARA ERRORES DE PARAMETROS
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> manejarErrorDeTipo(MethodArgumentTypeMismatchException ex){
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("mensaje", ex.getMessage());
+        return new ResponseEntity<>(respuesta, HttpStatus.BAD_REQUEST);
     }
 
     //PARA CUALQUIER OTRO ERROR
